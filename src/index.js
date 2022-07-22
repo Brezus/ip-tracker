042a1cfffc6bf9ea84693d7da9f39a0ee52d951e
Roshane Miller <millerroshane44@gmail.com>
2022-07-22 00:16:39 -0400

    made function which saves ip info in obj

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const searchBox = document.getElementById("ipTrackr")
 const submitButton = document.getElementById("submit")
-// const key = 'at_oNvwKc6F4I37eCbKER4WSoYriVKGY'
-const ipAddress = ""
+let ipAddress = ""
 const searchUrl = `https://geo.ipify.org/api/v2/country?apiKey=at_oNvwKc6F4I37eCbKER4WSoYriVKGY&ipAddress=${ipAddress}`
 
 function fetchIpData() {
@@ -12,9 +11,9 @@ function fetchIpData() {
       if (res.ok) {
         return res.json()
       }
-      throw new Error("supm nuh right")
+      throw new Error("something is not right")
     })
-    .then((data) => console.log(data))
+    .then((data) => displayIpInfo(data))
     .catch((error) => console.log(error))
 }
 
@@ -26,7 +25,20 @@ function fetchIpData() {
 // save value in ipAddress variable call fetchIpData
 // make function display
 // display returns div with information retrned from data
+// get ip address, location, timezone, isp
 
 function displayIpInfo(ipData) {
-  ipData
+  const info = {
+    ipAd: ipData.ip,
+    location: {
+      country: ipData.location.country,
+      region: ipData.location.region,
+      city: ipData.location.city,
+    },
+    timezone: ipData.location.timezone,
+    isp: ipData.isp,
+    coords: { lat: ipData.location.lat, lng: ipData.location.lng },
+  }
+  console.log(info)
 }
+submitButton.addEventListener("click", fetchIpData)
