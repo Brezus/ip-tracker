1da3d67c4afad1d8c7207cc72ad7feb343a487db
Roshane Miller <millerroshane44@gmail.com>
2022-07-23 20:42:25 -0400

    input focus on page load
    input gets focused on page load display class becomes error class if fetch res not ok if res ok then regular class gets added to display

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const searchBox = document.getElementById("ipTrackr")
 const submitButton = document.getElementById("submit")
 const display = document.querySelector(".info-display")
+const zoomLevel = 14
 let domainSearch = `https://geo.ipify.org/api/v2/country,city?apiKey=at_oNvwKc6F4I37eCbKER4WSoYriVKGY&domain=`
 let ipSearch = `https://geo.ipify.org/api/v2/country,city?apiKey=at_oNvwKc6F4I37eCbKER4WSoYriVKGY&ipAddress=`
 let searchUrl =
   "https://geo.ipify.org/api/v2/country,city?apiKey=at_oNvwKc6F4I37eCbKER4WSoYriVKGY&ipAddress=192.212.174.101"
 
-searchBox.addEventListener("input", setSearchUrl)
-document.addEventListener("DOMContentLoaded", fetchIpData)
+searchBox.addEventListener("keydown", (e) => {
+  setSearchUrl()
+  if (e.code === "Enter") {
+    fetchIpData()
+  }
+})
+document.addEventListener("DOMContentLoaded", () => {
+  searchBox.value = ""
+  searchBox.focus()
+  fetchIpData()
+  console.log("loaded")
+})
 function setSearchUrl() {
   const searchStr = searchBox.value.split(".").join("")
   searchUrl = !isNaN(searchStr)
@@ -15,16 +26,23 @@ function setSearchUrl() {
     : domainSearch + searchBox.value
 }
 
+function handleError() {
+  const errorMsg = `<p>couldnt find that search</p>`
+  display.className = "info-display-error"
+  display.innerHTML = errorMsg
+}
+
 function fetchIpData() {
   fetch(searchUrl)
     .then((res) => {
+      console.log(res)
       if (res.ok) {
         return res.json()
       }
       throw new Error("something is not right")
     })
     .then((data) => displayIpInfo(data))
-    .catch((error) => console.log(error))
+    .catch((error) => handleError())
 }
 
 function displayIpInfo(ipData) {
@@ -38,12 +56,13 @@ function displayIpInfo(ipData) {
     { title: "timezone", info: `UTC${ipData.location.timezone}` },
     { title: "isp", info: ipData.isp },
   ]
+  display.className = "info-display"
   display.innerHTML = getDisplayDivInnerHtml(ipInfo).join("")
   const lat = ipData.location.lat
   const lng = ipData.location.lng
-  map.flyTo(new L.LatLng(lat, lng), 8)
+  map.flyTo(new L.LatLng(lat, lng), zoomLevel)
   marker = L.marker([lat, lng], { icon: blackIcon }).addTo(map)
-  marker.bindPopup(`<b> you are ${ipData.location.region}</b>`)
+  marker.bindPopup(`<b>Welcome to the hotel ${ipData.location.region}</b>`)
 }
 function getDisplayDivInnerHtml(ipInfo) {
   const displayDivInnerHtml = ipInfo.map((data) => {
@@ -57,20 +76,20 @@ function getDisplayDivInnerHtml(ipInfo) {
   })
   return displayDivInnerHtml
 }
-submitButton.addEventListener("click", fetchIpData)
+submitButton.addEventListener("click", () => {
+  fetchIpData()
+  searchBox.value = ""
+})
 
-let map = L.map("map").setView([34.04915, -118.09462], 8)
+let map = L.map("map").setView([34.04915, -118.09462], zoomLevel)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "© OpenStreetMap",
 }).addTo(map)
 let blackIcon = L.icon({
   iconUrl: "images/icon-location.svg",
-  iconSize: [38, 50],
-  iconAnchor: [32, 24],
-  popupAnchor: [-3, -76],
+  iconSize: [30, 40],
+  iconAnchor: [32, 14],
+  popupAnchor: [-17, -5],
 })
 let marker = L.marker([34.04915, -118.09462], { icon: blackIcon }).addTo(map)
-marker.bindPopup(
-  "<b>Bungee Gum posses both the properties of rubber and gum</b>"
-)
